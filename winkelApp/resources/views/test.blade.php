148d8b76722bd6039a9dd528e3332d199778bf67
Leander <xx@xx.xx>
2017-09-06 13:24:21 +0200

    copy

---
+++ winkelApp/resources/views/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div></div>
+    <ul></ul>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aut eveniet deleniti dolorem nemo quaerat, id dicta adipisci reprehenderit dolorum numquam distinctio deserunt tempore quo perferendis dignissimos fugiat, dolore delectus esse consectetur corrupti. Optio corporis, necessitatibus itaque cum laborum sequi nostrum velit saepe culpa odit, minima harum officiis iusto maxime eius numquam dolor tenetur doloremque veritatis ipsum tempore exercitationem dolorum. Quas inventore doloremque explicabo consequuntur maxime dolore neque, iure temporibus quod consectetur amet, non accusamus cum ex aliquam voluptas commodi quasi perspiciatis omnis eaque vero. Ipsa sapiente ratione veritatis repellendus aliquam cum culpa cumque rerum exercitationem praesentium iusto laudantium, dignissimos totam fugit perferendis fugiat repudiandae at animi facilis doloremque laborum. Ea, nulla? Necessitatibus sunt consequatur molestias pariatur, deserunt hic libero qui consectetur ad ducimus commodi suscipit iusto assumenda delectus aspernatur, sapiente placeat vero magni nesciunt earum iure nulla asperiores. Ab nam ea inventore dolor quisquam nihil accusantium maiores! Blanditiis, perspiciatis. Soluta a dolorum provident sapiente cumque nihil modi ab voluptates debitis assumenda nesciunt perferendis rerum dolores dolore obcaecati eius, culpa distinctio. Magni, culpa nemo officiis voluptatem soluta a doloribus dolorum consequatur fugiat voluptas. Fugiat quis vitae, reiciendis eius non velit sit error iste officia dignissimos? Architecto recusandae illum nihil accusamus.</p>
+   
+</body>
+</html>
